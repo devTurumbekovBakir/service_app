b03901064378666ce96a84216b1db439967fac08
turumbekov bakir <bakirturumbekov34@gmail.com>
2023-10-30 18:48:49 +0600

    added task set_price

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch, F, Sum
+from django.db.models import Prefetch, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -12,7 +12,7 @@ class SubscriptionViewSet(ReadOnlyModelViewSet):
         Prefetch('client',
                  queryset=Client.objects.all().select_related('user').only('company_name',
                                                                            'user__email'))
-    ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.0)
+    )
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
