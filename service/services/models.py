28cbdfa275b3043e8d31682beec5af6d1cc19ab1
turumbekov bakir <bakirturumbekov34@gmail.com>
2023-10-31 18:40:02 +0600

    added manual cache

--- service/services/models.py
+++ service/services/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
+from django.db.models.signals import post_delete
 
 from clients.models import Client
+from .receivers import delete_cache_total_sum
 from .tasks import set_price, set_comment
 
 
@@ -63,3 +65,13 @@ class Subscription(models.Model):
 
     def __str__(self):
         return f'{self.client} {self.plan}'
+
+    def save(self, *args, **kwargs):
+        creating = not bool(self.id)
+        result = super().save(*args, **kwargs)
+        if creating:
+            set_price.delay(self.id)
+        return result
+
+
+post_delete.connect(delete_cache_total_sum, sender=Subscription)
--- service/services/tasks.py
+++ service/services/tasks.py
@@ -3,6 +3,8 @@ import datetime
 
 from celery import shared_task
 from celery_singleton import Singleton
+from django.conf import settings
+from django.core.cache import cache
 from django.db import transaction
 from django.db.models import F
 
@@ -12,16 +14,14 @@ def set_price(subscription_id):
     from services.models import Subscription
 
     with transaction.atomic():
-        time.sleep(5)
-
         subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
             annotated_price=F('service__full_price') -
                   F('service__full_price') * F('plan__discount_percent') / 100.0).first()
 
-        time.sleep(20)
-
         subscription.price = subscription.annotated_price
         subscription.save()
+
+    cache.delete(settings.PRICE_CACHE_NAME)
 
 
 @shared_task(base=Singleton)
@@ -31,7 +31,7 @@ def set_comment(subscription_id):
     with transaction.atomic():
         subscription = Subscription.objects.select_for_update().get(id=subscription_id)
 
-        time.sleep(27)
-
         subscription.comment = str(datetime.datetime.now())
         subscription.save()
+
+    cache.delete(settings.PRICE_CACHE_NAME)
